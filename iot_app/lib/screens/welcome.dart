912e9d394fd966e6088cb1e971959065ce7a0e8e
miklimek <miklimek@student.agh.edu.pl>
2022-12-30 19:14:52 +0100

    Notifications
    allows sending notifications using aws console

--- iot_app/lib/screens/welcome.dart
+++ iot_app/lib/screens/welcome.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iot_app/screens/register.dart';
+<<<<<<< Updated upstream
+=======
+import 'login.dart';
+import 'package:iot_app/features/notifications.dart';
+import 'package:iot_app/firebase_options.dart';
+
+// Amplify Flutter Packages
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_api/amplify_api.dart'; // UNCOMMENT this line after backend is deployed
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+>>>>>>> Stashed changes
 
 import 'login.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class WelcomePage extends StatefulWidget {
   WelcomePage({Key? key, this.title}) : super(key: key);
@@ -14,6 +30,71 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+<<<<<<< Updated upstream
+=======
+  @override
+  void initState() {
+    // kick off app initialization
+    _initializeApp();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // to be filled in a later step
+  }
+
+  Future<void> _initializeApp() async {
+    // configure Amplify
+    await _configureAmplify();
+    await _configureFirebase();
+
+    // after configuring Amplify, update loading ui state to loaded state
+    setState(() {});
+  }
+
+  Future<void> _configureFirebase() async {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+    NotificationHandler? _notificationHandler = NotificationHandler();
+    try {
+      FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+    } catch (e) {
+      print(e);
+    }
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (message.data != null) {
+        _notificationHandler.firebaseMessagingForegroundHandler(message);
+      }
+    });
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+
+      // amplify plugins
+      final _dataStorePlugin = AmplifyDataStore(modelProvider: ModelProvider.instance);
+
+      // Add the following line and update your function call with `addPlugins`
+      final api = AmplifyAPI();
+
+      // add Amplify plugins
+      await Amplify.addPlugins([_dataStorePlugin, api, AmplifyAnalyticsPinpoint(), AmplifyAuthCognito()]);
+
+      // configure Amplify
+      //
+      // note that Amplify cannot be configured more than once!
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+
+      // error handling can be improved for sure!
+      // but this will be sufficient for the purposes of this tutorial
+      safePrint('An error occurred while configuring Amplify: $e');
+    }
+  }
+
+>>>>>>> Stashed changes
   Widget _submitButton() {
     return InkWell(
       onTap: () {
@@ -53,7 +134,7 @@ class _WelcomePageState extends State<WelcomePage> {
         padding: EdgeInsets.symmetric(vertical: 13),
         alignment: Alignment.center,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
             boxShadow: <BoxShadow>[
               BoxShadow(
                   color: Color(0xff1c98ad).withAlpha(100),
@@ -72,8 +153,8 @@ class _WelcomePageState extends State<WelcomePage> {
 
   Widget _title() {
     return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
+        textAlign: TextAlign.center,
+        text: TextSpan(
           text: 'IoT Projekt',
           style: GoogleFonts.portLligatSans(
             textStyle: Theme.of(context).textTheme.headline1,
@@ -81,7 +162,7 @@ class _WelcomePageState extends State<WelcomePage> {
             fontWeight: FontWeight.w700,
             color: Colors.white,
           ),
-      )
+        )
     );
   }
 
